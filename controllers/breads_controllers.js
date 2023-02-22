c538a9556305b11b5aaec0b31bc5077714b0a4f1
Luis <luisrc91@live.com>
2023-02-22 18:52:17 -0500

    added css

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -12,8 +12,18 @@ breads.get('/', (req, res) => {
 })
 
 // SHOW
+// SHOW
+// SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
+
+
   
 module.exports = breads
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -19,9 +20,12 @@ app.get('/', (req, res) => {
   // Breads
   const breadsController = require('./controllers/breads_controllers.js')
   app.use('/breads', breadsController)
-  
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
+})
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
 })
